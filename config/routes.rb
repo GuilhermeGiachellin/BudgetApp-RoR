4a73af3b824636f70abaa2df4a62056ec0ae0bb9
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-11-30 14:42:33 -0300

    Routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'entities#index'
   resources :entities
   resources :groups
   resources :users
-  root 'devise/registrations#new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
