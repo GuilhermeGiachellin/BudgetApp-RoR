b76f4a729b974e237aa3cd49d4ad644e86625736
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-12-02 11:01:02 -0300

    Add icons for diferente categories

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,14 @@
 module GroupsHelper
+    def list_icons
+        [
+          ['Travel', 'travel.png'],
+          ['Car', 'car.png'],
+          ['Games', 'console.png'],
+          ['Books', 'books.png'],
+          ['Fitness', 'Dumbbell.png'],
+          ['Entertainment', 'entertainment.png'],
+          ['Food', 'food.png'],
+          ['Home', 'home.png'],
+        ]
+      end
 end
