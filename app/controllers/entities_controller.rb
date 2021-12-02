53b10bcf75b92900758d2ca0faa491e5d08e46fd
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-12-02 10:04:47 -0300

    Change alert message

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -20,7 +20,7 @@ class EntitiesController < ApplicationController
    
     respond_to do |format|
       if @entity.save
-        format.html { redirect_to group_entities_path, notice: "Entity was successfully created." }
+        format.html { redirect_to group_entities_path, notice: "Transaction created." }
         format.json { render :show, status: :created, location: @entity }
       else
         format.html { render :new, status: :unprocessable_entity }
